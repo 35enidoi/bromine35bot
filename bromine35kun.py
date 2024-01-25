04d0124b3c37e374377e95b12c03614cea3cc6b1
いおぢね <143810964+35enidoi@users.noreply.github.com>
2024-01-25 21:40:59 +0900

    api_postの修正

--- bromine35kun.py
+++ bromine35kun.py
@@ -220,7 +220,7 @@ async def onreversi(info):
 async def api_post(endp:str, wttime:int, **dicts) -> requests.Response:
     url = f"https://{INSTANCE}/api/"+endp
     dicts["i"] = TOKEN
-    return await asyncio.to_thread(requests.post(url, json=dicts, timeout=wttime))
+    return await asyncio.to_thread(requests.post, url, json=dicts, timeout=wttime)
 
 async def create_reaction(id,reaction,Instant=False):
     if not Instant:
